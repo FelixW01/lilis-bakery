7e41efb13b8ba3d58dba95ae41cf3a6bd55d2619
Felix W <felixwillem01@yahoo.com>
2024-01-03 20:38:57 -0500

    worked on both userRoutes and foodRoutes

--- server/routes/api/foodRoutes.js
+++ server/routes/api/foodRoutes.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Food = require("../../models/Food.js");
 
 // Define your API endpoints here
-router.get('/food', (req, res) => {
+router.get('/food', async (req, res) => {
     // Handle GET request for /api/food
+    try {
+        const food = await Food.find();
+        res.send(food);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error)
+    }
     res.send(food);
 });
 
 router.post("/food", async (req, res) => {
+    const {name, price, count, weight, quantity, img, description, ingridients} = req.body;
 	const newFood = new Food({
-		name: req.body.name,
-		price: req.body.price,
-
+		name,
+        price,
+        count,
+        weight,
+        quantity,
+        img,
+        description,
+        ingridients
 	});
 	await newFood.save((err, food) => {
 		if (err) {
