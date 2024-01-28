c9a87d28b83f1be1bbe2dc8204591b3754f82cf3
Felix W <felixwillem01@yahoo.com>
2024-01-27 21:33:56 -0500

    worked on polishing navbar and homepage.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
 import styles from "./Header.module.css";
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo-light.png';
+import { useLocation } from 'react-router-dom';
 
 export default function Header() {
+  const location = useLocation();
+
   return (
     <>
    <nav className={styles.navbar}>
-      <div className={styles.navDiv}>
-          <Link to={'/'}>
-            <img src={logo} alt='logo' className={styles.logo}/>
-          </Link>
+      <div className={styles.logoDiv}>
+      {location.pathname === "/" ? 
+      <a href="#main"><img src={logo} alt='logo' className={styles.logo}></img></a> 
+      : <Link to={'/'}>
+          <img src={logo} alt='logo' className={styles.logo}/>
+          </Link>}
       </div>
       <div className={styles.navDiv}>
             <Link to='/login' className={styles.navLink}>Login</Link>
