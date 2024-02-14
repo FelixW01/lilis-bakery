24057b2f5bb33a05ffae4b06ec8db9df441bb1dd
Felix W <felixwillem01@yahoo.com>
2024-02-13 21:13:17 -0500

    worked on setting up profile and order pages

---
+++ client/src/components/Orders/OrdersPage.jsx
@@ -0,0 +1,13 @@
+import styles from "./OrdersPage.module.css";
+
+export default function OrdersPage() {
+  return (
+    <>
+    <div className={styles.container}>
+      <div>
+        <h2>Your Orders</h2>
+      </div>
+    </div>
+    </>
+  );
+}
