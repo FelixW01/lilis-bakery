3ad167f3667092ad96e933e8f2afb6991d4ff367
Felix W <felixwillem01@yahoo.com>
2024-02-25 19:36:49 -0500

    worked with fetching cart data via axios and created card for cart content

--- client/src/components/CartPage/CartPage.jsx
+++ client/src/components/CartPage/CartPage.jsx
@@ -1,11 +1,37 @@
 import styles from "./CartPage.module.css";
+import React, { useState, useEffect, useContext} from "react";
+import axios from "axios";
+import { UserContext } from "../../../context/userContext";
+import { Card } from 'antd';
 
 export default function CartPage() {
+
+  const [cart, setCart] = useState([]);
+  const {user} = useContext(UserContext)
+
+  useEffect(() => {
+    const fetchCartData = async () => {
+      try {
+        const response = await axios.get(`/cart`);
+        setCart(response.data)
+      } catch (error) {
+        console.error('Error fetching cart data', error.message)
+      }
+    }
+    fetchCartData()
+  },[])
+
+  console.log(cart)
+
   return (
     <>
     <div className={styles.cartContainer}>
       <div className={styles.cardDiv}>
-        <h2>Your Basket is empty.</h2>
+        <Card title="Card title" bordered={false} style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
       </div>
     </div>
     </>
