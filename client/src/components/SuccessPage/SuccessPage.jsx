65b37904b74a7c4d414a606849941cb1e582a13b
Felix W <felixwillem01@yahoo.com>
2024-03-06 22:17:21 -0500

    worked on implimenting cart deletion and order creation on successfull checkout

--- client/src/components/SuccessPage/SuccessPage.jsx
+++ client/src/components/SuccessPage/SuccessPage.jsx
@@ -1,6 +1,23 @@
 import styles from "./SuccessPage.module.css";
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
 
 export default function SuccessPage() {
+
+useEffect(() => {
+    // Make a GET request to the "/success" endpoint
+    axios.get('/checkout/success')
+      .then(response => {
+        // Handle the success response
+        console.log('Successfully reached the /success endpoint', response.data);
+      })
+      .catch(error => {
+        // Handle errors
+        console.error('Error reaching the /success endpoint', error);
+      });
+  }, []); 
+
   return (
     <>
     <div className={styles.container}>
