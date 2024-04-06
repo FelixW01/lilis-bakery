b22854416586d01746b737a33ae1e03f950abdd0
Felix W <felixwillem01@yahoo.com>
2024-04-06 19:56:22 -0400

    worked on populating latest order upon a successful payment

--- client/src/components/SuccessPage/SuccessPage.jsx
+++ client/src/components/SuccessPage/SuccessPage.jsx
@@ -2,6 +2,7 @@ import styles from "./SuccessPage.module.css";
 import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import { UserContext } from "../../../context/userContext";
+import { Card } from 'antd';
 
 export default function SuccessPage() {
 
@@ -9,6 +10,14 @@ const token = localStorage.getItem('token');
 const [loading, setLoading] = useState(true)
 const [cart, setCart] = useState([]);
 const {user} = useContext(UserContext)
+const [latestOrder, setLatestOrder] = useState(null);
+
+
+    // Function to format the date string
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
 
   const handleDeleteCart = async () => {
     const headers = {
@@ -53,6 +62,30 @@ const {user} = useContext(UserContext)
       console.error('Error creating order', error.message);
   }
 };
+   // Grabs latest order information on mount
+  const fetchLatestOrder = async () => {
+      try {
+        const headers = {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        };
+
+        const response = await axios.get('/order/latest', {
+          withCredentials: true,
+          headers: headers,
+        });
+
+        if (response.status === 200) {
+          setLatestOrder(response.data.order);
+          setLoading(false);
+        } else {
+          console.error('Error fetching latest order', response.data.message);
+        }
+      } catch (error) {
+        console.error('Error fetching latest order', error.message);
+      }
+  };
+
 
   useEffect(() => {
     const fetchCartData = async () => {
@@ -81,6 +114,8 @@ const {user} = useContext(UserContext)
         if(cart && cart.data && cart.data.isPaid) {
           handleCreateOrder(); // Call handleCreateOrder only if payment is successful
           handleDeleteCart(); // Call handleDeleteOrder only if payment is successful
+          // Fetch latest order after creating and deleting cart
+          fetchLatestOrder();
         }
       } else {
         console.error('Error fetching cart data: Unexpected response structure', response.data);
@@ -100,7 +135,18 @@ const {user} = useContext(UserContext)
   return (
     <div className={styles.container}>
       <div>
+        {!loading && latestOrder ? 
+        <>
         <h2>Payment Successful!</h2>
+        <Card key={latestOrder._id} title={latestOrder.items[0].name} bordered={false} style={{ width: 300 }} className={styles.orderCard}>
+                  <p>Order Placed: {formatDate(latestOrder.createdAt)}</p>
+                  <p>Total: ${latestOrder.subTotal}</p>
+                  <p>Order ID: {latestOrder._id}</p>
+                  <div className={styles.imgDiv}>
+                    <img src="images/nastar.png" alt="nastar" style={{ width: 150, height: 150 }} />
+                  </div>
+        </Card> 
+        </> : <h1>Loading ...</h1>}
       </div>
     </div>
   );
